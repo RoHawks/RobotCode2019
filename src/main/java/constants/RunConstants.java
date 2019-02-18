f91249fbfc195d1e8664ed75af66e580ce3a094a
danielchao0214 <34559279+danielchao0214@users.noreply.github.com>
2019-02-18 12:56:39 -0500

    Sync part 1

--- src/main/java/constants/RunConstants.java
+++ src/main/java/constants/RunConstants.java
@@ -1,7 +1,8 @@
 package constants;
 
 public class RunConstants {
-	public static boolean
+
+	public static final boolean
 		RUNNING_DRIVE = true,
 		RUNNING_PNEUMATICS = true,
 		RUNNING_LEADSCREW = true,
@@ -9,10 +10,12 @@ public class RunConstants {
 		RUNNING_BALL = RUNNING_PNEUMATICS && true,
 		RUNNING_CLIMBER = false,
 		RUNNING_CAMERA = true,
+	
 		
 		SECONDARY_JOYSTICK = true, // using the box instead of logitech attack
 		IS_PROTOTYPE = false,
 		LOGGING = false,
 		
 		RUNNING_EVERYTHING = true && RUNNING_DRIVE && RUNNING_PNEUMATICS && RUNNING_LEADSCREW && RUNNING_HATCH && SECONDARY_JOYSTICK;
+		
 }
