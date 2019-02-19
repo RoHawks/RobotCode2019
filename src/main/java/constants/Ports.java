e05e27af252c53ab09e3b8eea5cc16118a51fbb1
danielchao0214 <34559279+danielchao0214@users.noreply.github.com>
2019-02-18 20:13:17 -0500

    navx port

--- src/main/java/constants/Ports.java
+++ src/main/java/constants/Ports.java
@@ -8,7 +8,7 @@ public class Ports {
 	//*******************//
 	// GENERAL VARIABLES //
 	//*******************//
-	public static final SerialPort.Port NAVX = Port.kMXP;
+	public static final SerialPort.Port NAVX = Port.kUSB;
 
 	public static final int
 		//Controllers
